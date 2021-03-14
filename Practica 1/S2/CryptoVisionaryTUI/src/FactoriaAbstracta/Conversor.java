3d16c2af86f290e0b3523a074292c6a0975e2b91
GFernando97 <angr7991@hotmail.com>
2021-03-14 18:39:29 +0100

    Archivos reestructurados

---
+++ Practica 1/S2/CryptoVisionaryTUI/src/FactoriaAbstracta/Conversor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoriaAbstracta;
+
+/**
+ *
+ * @author reko98D
+ */
+public class Conversor {
+   protected String Divisa;
+   protected double valorUnidad;
+    
+    
+    public void run(){
+        
+    }
+    
+}
---
+++ Practica 1/S2/CryptoVisionaryTUI/src/FactoriaAbstracta/CryptoCurrency.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoriaAbstracta;
+
+
+public class CryptoCurrency {
+    protected String token;
+    protected Double valorActual;
+    protected Double valorMinimo;
+    protected Double valorMaximo;
+    
+    
+    
+    public void setToken(){
+        
+    }
+    
+    public void setValorActual(double nuevoValor){
+      this.valorActual=nuevoValor; 
+    }
+    
+    public void setValorMaximo(double nuevoValor){
+        this.valorMaximo=nuevoValor;
+    }
+    
+    public void setValorMinimo(double nuevoValor){
+        this.valorMinimo=nuevoValor;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Double getValorActual() {
+        return valorActual;
+    }
+
+    public Double getValorMinimo() {
+        return valorMinimo;
+    }
+
+    public Double getValorMaximo() {
+        return valorMaximo;
+    }
+    
+    
+    
+    public void run(){
+        
+    }
+    
+    
+}
