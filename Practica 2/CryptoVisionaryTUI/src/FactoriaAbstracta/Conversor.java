ff06dda12a35bdcdb0a884eb35bad14378d4553f
GFernando97 <angr7991@hotmail.com>
2021-03-14 17:43:35 +0100

    P2 folder

---
+++ Practica 2/CryptoVisionaryTUI/src/FactoriaAbstracta/Conversor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoriaAbstracta;
+
+/**
+ *
+ * @author reko98
+ */
+public class Conversor {
+    String Divisa;
+    double valorUnidad;
+    
+    
+    public void run(){
+        
+    }
+    
+}
---
+++ Practica 2/CryptoVisionaryTUI/src/FactoriaAbstracta/Ethereum.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoriaAbstracta;
+
+/**
+ *
+ * @author reko98
+ */
+public class Ethereum extends CryptoCurrency {
+    
+    @Override
+     public void setToken(){
+        this.token="ETH";
+    }
+     
+     
+    @Override
+    public void run(){
+        
+    }
+    
+}
