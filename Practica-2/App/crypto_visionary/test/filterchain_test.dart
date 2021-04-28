e26017c201a7025d00a6282ebc9b889f42389eef
mnc99 <moises.noguera1999@gmail.com>
2021-04-28 11:00:11 +0200

    Test FilterChain Finalizado (añadidos resultados)

--- Practica-2/App/crypto_visionary/test/filterchain_test.dart
+++ Practica-2/App/crypto_visionary/test/filterchain_test.dart
@@ -1,16 +1,17 @@
 import 'package:crypto_visionary/Cryptocurrency.dart';
+import 'package:crypto_visionary/FIlterInvestment.dart';
 import 'package:crypto_visionary/FilterHalving.dart';
 import 'package:test/test.dart';
 import 'package:crypto_visionary/FilterChain.dart';
 
 void main() {
   group('FilterChain', () {
-    test('Inicialmente nuevoPrecio es nulo', () {
+    test('Inicialmente el nuevo precio de la criptomoneda tiene valor 0.0', () {
       final filterChain = FilterChain.getInstance();
       expect(filterChain.nuevoPrecio, 0.0);
     });
 
-    test('El valor no debe ser 0.0', () {
+    test('El nuevo precio de la criptomoneda deber ser 48048.0', () {
       final moneda = new Cryptocurrency("BTC", 3000000000, 48000, 12000, 64000);
       final filterChain = FilterChain.getInstance();
       filterChain.addFilter(new FilterHalving(1.001));
@@ -22,6 +23,30 @@ void main() {
       aplicar el filtro de Halving deber ser 48048
        */
       expect(filterChain.nuevoPrecio, 48048.0);
+
+    });
+
+    test('El número de filtros actual es 1', () {
+
+      final filterChain = FilterChain.getInstance();
+      var numFiltros = filterChain.filterList.length;
+
+      //Inicialmente, la lista de filtros tiene un filtro,
+      //el añadido en el test anterior
+      expect(numFiltros, 1);
+
+    });
+
+    test('El número de filtros deber ser 3', () {
+
+      final filterChain = FilterChain.getInstance();
+      filterChain.addFilter(new FilterHalving(1.001));
+      filterChain.addFilter(new FilterInvestment(50000));
+      var numFiltros = filterChain.filterList.length;
+
+      //Se han añadido dos filtros más, por tanto el tamaño
+      //de la lista de filtros deber ser de 3
+      expect(numFiltros, 3);
 
     });
   });
